e44698979fc8652622513e9b231da81f56c43407
Murhussain <husseinmurashi@yahoo.com>
2023-02-20 14:56:07 +0200

    Done with header section of app

--- components/header.js
+++ components/header.js
@@ -20,7 +20,7 @@ export default function Header() {
         <div className='flex ml-2 items-center rounded-full bg-gray-100 p-2'>
           <SearchIcon className='h-6 text-gray-600'/>
           <input
-            className='flex items-center ml-2 bg-transparent outline-none placeholder-gray-500 flex-shrink'
+            className='hidden md:inline-flex items-center ml-2 bg-transparent outline-none placeholder-gray-500 flex-shrink'
             type="text" 
             placeholder='Search Facebook'
           />
@@ -30,7 +30,7 @@ export default function Header() {
       {/* header center */}
       <div className='flex  justify-center flex-grow'>
         <div className='flex space-x-6 md:space-x-2'>
-          <HeaderIcon Icon={HomeIcon} />
+          <HeaderIcon active Icon={HomeIcon} />
           <HeaderIcon Icon={FlagIcon} />
           <HeaderIcon Icon={PlayIcon} />
           <HeaderIcon Icon={ShoppingCartIcon} />
@@ -39,10 +39,13 @@ export default function Header() {
       </div>
 
       {/* header right */}
-      <div>
-        {/* profile image */}
-
-        <p>My name</p>
+      <div className='flex items-center sm:space-x-2 justify-end'>
+        {/* profile pic utilizing nextAuth */}
+        <p className='whitespace-nowrap font-semibold pr-3'>My name</p>
+        <ViewGridIcon className='icon'/>
+        <ChatIcon className='icon'/>
+        <BellIcon className='icon' />
+        <ChevronDownIcon className='icon' />
       </div>
     </div>
   );
